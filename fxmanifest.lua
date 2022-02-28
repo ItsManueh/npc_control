27c72804547984396b65014e0c87d55099cb1a10
ItsManueh <47420352+ItsManueh@users.noreply.github.com>
2022-02-28 16:03:54 +0100

    Upload fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+--#### FX Information ####--
+fx_version   'adamant'
+use_fxv2_oal 'yes'
+lua54        'yes'
+game         'gta5'
+
+--#### Resource Information ####--
+name         'ReducedNPC'
+Author       'ItsManueh'
+version      '1.0'
+description  'Powered by Uranium'
+
+client_script 'cl.lua'
+
+server_script 'sv.lua'
